11c2f7ee2a02a1984f1c865ae7ff361fd75cc0c2
Jon <jonsweeney26@gmail.com>
2016-03-31 16:42:06 -0700

    add some update info

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateRentalForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        date: this.get('date'),
+        title: this.get('title'),
+        image: this.get('image'),
+        blog: this.get('blog')
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
 });
